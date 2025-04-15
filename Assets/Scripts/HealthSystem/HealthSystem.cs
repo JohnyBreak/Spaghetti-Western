4123b7d3da9c3a95df3229db5955b51f1a5bf67a
konstantin.rashevskiy <konstantin.r@danko.team>
2025-04-15 23:38:11 +0700

    начало для отдачи
    поворот туловища при прицеливании и начало для отдачи

--- Assets/Scripts/HealthSystem/HealthSystem.cs
+++ Assets/Scripts/HealthSystem/HealthSystem.cs
@@ -1,8 +1,5 @@
-
 using System;
 using UnityEngine;
-using UnityEngine.Events;
-//using Zenject;
 
 public class HealthSystem : MonoBehaviour, IDamageable, IHealable
 {
---
+++ Assets/WeaponSystem/Scripts/Recoil/RecoilsContainer.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace WeaponSystem
+{
+    public class RecoilsContainer : MonoBehaviour
+    {
+        [SerializeField] private Transform _rightHand;
+        [SerializeField] private Transform _leftHand;
+        [SerializeField] private Transform _twoHanded;
+
+    }
+}
